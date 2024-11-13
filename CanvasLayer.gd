ff85fdf675fce5db295ea711bdc3afdb26ac6bd5
Paul0Parenti <paulo_parenti@usp.br>
2024-11-12 23:52:56 -0300

    feat: adds 'weather' panel

---
+++ CanvasLayer.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+@onready var texture_rect = $Weather/TextureRect
+const FIRE_SYMBOL = preload("res://assets/PNG/fire-symbol.jpg")
+const WATER_SYMBOL = preload("res://assets/PNG/water_drop.jpg")
+const WIND_SYMBOL = preload("res://assets/PNG/wind-symbol.jpg")
+
+
+func _ready():
+	texture_rect.set_texture(WIND_SYMBOL)
+
+
+func _process(delta):
+	change_weather()
+
+func change_weather():
+	if Input.is_action_just_pressed("Change Skill I", true) :
+		texture_rect.set_texture(WATER_SYMBOL)
+		
+	if Input.is_action_just_pressed("Change Skill II", true) :
+		texture_rect.set_texture( FIRE_SYMBOL)
+		
+	if Input.is_action_just_pressed("Change Skill III", true):
+		texture_rect.set_texture(WIND_SYMBOL)
