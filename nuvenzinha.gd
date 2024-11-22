b4e7e003c9235b81e0ece286130b0608aac4c397
Marcos-Paulo-Pacifico <paulopacific.pp@gmail.com>
2024-11-21 23:48:53 -0300

    adicionados efeitos de skill

--- nuvenzinha.gd
+++ nuvenzinha.gd
@@ -3,13 +3,18 @@ extends CharacterBody2D
 @export var MAX_SPEED = 200
 @export var ACCELERATION = 1500
 @export var FRICTION = 1200
+@export var SKILL_EFFECT_TIMER = 2
 @onready var gpu_particles_2d = $GPUParticles2D
 @onready var gpu_particles_2d_2 = $GPUParticles2D2
 @onready var axis = Vector2.ZERO
+@onready var chuvaCaindo = $ChuvaCaindo
+@onready var ventoSoprando = $VentoSoprando
+@onready var solRaiando = $SolRaiando
 
 const WINDY = preload("res://assets/PNG/nuvenzinha_particula_textura.png")
 const RAINY = preload("res://assets/PNG/nuvenzinha_particula_chuva_textura.png")
 
+var skillState = "windy"
 
 func _ready():
 	gpu_particles_2d.set_texture(WINDY)
@@ -20,6 +25,7 @@ func _physics_process(delta):
 	if gpu_particles_2d.is_visible_in_tree():
 		generate_fluffy()
 	change_skill()
+	skill_effect()
 	
 func get_input_axis():
 	axis.x = int(Input.is_action_pressed("Right", true)) - int(Input.is_action_pressed("Left", true))
@@ -55,12 +61,32 @@ func change_skill():
 		gpu_particles_2d.show()
 		gpu_particles_2d_2.hide()
 		gpu_particles_2d.set_texture(RAINY)
-
+		skillState = "rainy"
+		
 	if Input.is_action_just_pressed("Change Skill II", true) :
 		gpu_particles_2d.hide()
 		gpu_particles_2d_2.show()
+		skillState = "sunny"
 		
 	if Input.is_action_just_pressed("Change Skill III", true):
 		gpu_particles_2d.show()
 		gpu_particles_2d_2.hide()
 		gpu_particles_2d.set_texture(WINDY)
+		skillState = "windy"
+
+func skill_effect():
+	if Input.is_action_just_pressed("Skill", true) and skillState == "rainy":
+		chuvaCaindo.show()
+		await get_tree().create_timer(SKILL_EFFECT_TIMER).timeout
+		chuvaCaindo.hide()
+	
+	if Input.is_action_just_pressed("Skill", true) and skillState == "windy":
+		ventoSoprando.show()
+		await get_tree().create_timer(SKILL_EFFECT_TIMER).timeout
+		ventoSoprando.hide()
+		
+	if Input.is_action_just_pressed("Skill", true) and skillState == "sunny":
+		solRaiando.show()
+		await get_tree().create_timer(SKILL_EFFECT_TIMER).timeout
+		solRaiando.hide()
+		
